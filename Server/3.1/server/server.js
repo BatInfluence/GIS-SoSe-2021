9ef6e74ac72279e4a7a18b24f19784e31ceda638
BatInfluence <carolin.messerle@hs-furtwangen.de>
2021-05-30 17:33:41 +0200

    fertig?

--- Server/3.1/server/server.js
+++ Server/3.1/server/server.js
@@ -1,15 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Aufgabe3_1 = void 0;
-const Http = require("http");
+const Http = require("http"); //sämtl Funktionalitäten werden importiert → Zugriff auf Funktionen und Objekte der Modeule
 var Aufgabe3_1;
 (function (Aufgabe3_1) {
     console.log("Starting server");
-    let port = Number(process.env.PORT);
-    if (!port)
-        port = 8100;
+    let port = Number(process.env.PORT); //processenvPORT → liefert Informationen zum Port
+    if (!port) //Wenn Port nicht geöffnet werden kann wird / geöffnet
+        port = 8100; //Kommen auf unsere HerokuSeite mit dem /
+    //Create new Server
     let server = Http.createServer();
-    server.addListener("request", handleRequest);
+    server.addListener("request", handleRequest); //Eingetipptes wird gespeichert
     server.addListener("listening", handleListen);
     server.listen(port);
     function handleListen() {
@@ -17,9 +18,9 @@ var Aufgabe3_1;
     }
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Header wird erstellt
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //jeder hat access
+        _response.write(_request.url); //IncomingMessage & ServerResponse
         _response.end();
     }
 })(Aufgabe3_1 = exports.Aufgabe3_1 || (exports.Aufgabe3_1 = {}));
--- Server/3.1/server/server.ts
+++ Server/3.1/server/server.ts
@@ -1,14 +1,15 @@
-import * as Http from "http";
 
+import * as Http from "http"; //sämtl Funktionalitäten werden importiert → Zugriff auf Funktionen und Objekte der Modeule
 export namespace Aufgabe3_1 {
     console.log("Starting server");
-    let port: number = Number(process.env.PORT);
-    if (!port)
-        port = 8100;
+    let port: number = Number(process.env.PORT); //processenvPORT → liefert Informationen zum Port
+    if (!port) //Wenn Port nicht geöffnet werden kann wird / geöffnet
+        port = 8100; //Kommen auf unsere HerokuSeite mit dem /
 
+    //Create new Server
     let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest); //Eingetipptes wird gespeichert
+    server.addListener("listening", handleListen); 
     server.listen(port);
 
     function handleListen(): void {
@@ -18,9 +19,9 @@ export namespace Aufgabe3_1 {
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Header wird erstellt
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //jeder hat access
+        _response.write(_request.url); //IncomingMessage & ServerResponse
         _response.end();
     }
 }
