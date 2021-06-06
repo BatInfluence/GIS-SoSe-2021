9b8e77ec60550034b7c4cfc623f6ba36eee3580d
BatInfluence <76529104+BatInfluence@users.noreply.github.com>
2021-06-06 18:10:28 +0200

    es funktioniert!! yay

--- Server/3.2/client/script.ts
+++ Server/3.2/client/script.ts
@@ -1,16 +1,43 @@
 namespace Aufgabe3_1 {
-    let buttonSend: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button2");
-    buttonSend.addEventListener("click", click); 
+    let url: string = "http://localhost:8100/";
+    let type: string = "";
 
-    let url: string = "contact.html";
-    async function click(): Promise<void> {
+    let htmlSubmit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("htmlsubmit");
+    let jsonSubmit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("jsonsubmit");
+    let responseDIV: HTMLDivElement = <HTMLDivElement>document.getElementById("responseDIV");
+    let buttonSend: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button2");
+
+    buttonSend.addEventListener("click", handleSubmit);
+
+    jsonSubmit.addEventListener("click", function (): void {
+        type = "json";
+        handleSubmit();
+    });
+
+    htmlSubmit.addEventListener("click", function (): void {
+        type = "html";
+        handleSubmit();
+    });
+
+    async function handleSubmit(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
-        //let url: string = "https://gis-sose2021.herokuapp.com/";
+        url += type;
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += "?" + query.toString();
         let response: Response = await fetch(url, { method: "get" });
         let responseText: string = await response.text();
-        alert(responseText);
+        alert(response.text);
+
+        if (type == "/json") {
+            let responseJSON: JSON = JSON.parse(responseText);
+            console.log(responseJSON);
+        }
+        else if (type == "/html") {
+            responseDIV.innerHTML = "";
+            let frag: DocumentFragment = document.createRange().createContextualFragment(responseText);
+            responseDIV.appendChild(frag);
+        }
+        url = "http://localhost:8100/";
     }
 }
